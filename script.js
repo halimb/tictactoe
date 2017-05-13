34783265762a1f40b3cf67ed4b41d3a0748cd631
Halim <halim.bouguedra@gmail.com>
2017-05-13 11:58:03 +0100

    implement the board object

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 
 function Board(cells) {
-	this.cells = cells;;
+	this.cells = cells;
+	this.terminal = false;
 	this.len = this.cells.length;
-
-	/*checks for diagonal lines, 
-	  if (fwd) checks for forward diagonal line, 
-	  else, checks for backwards diagonal */
-	this.checkDiagonal = function(fwd, value) {
-		var tempCells = fwd ? 
-			this.cells.slice().reverse() : this.cells;
-		for(var i = 0; i < this.len; i++) {
-			if(tempCells[i][i] != value) {
-				return false;;
-			}
-		}
-		return true;
-	}
-
-	//ckecks for orthogonal lines (vertical or horizontal)
-	this.checkOrthogonal = function(pos, value, vertical) {
-		for(var i = 0; i < this.len; i++) {
-			var cell = vertical ? 
-				this.cells[i][pos] :
-				this.cells[pos][i]
-			if(cell != value) {
-				return false;
-			}
-		}
-		return true;
-	}
 
 	/* scans the whole board for any kind of line.
 	   returns an object representing the first line 
 	   if any, or false if none */ 
 	this.checkLine = function(value) {
+
+		/*checks for diagonal lines, 
+		  if (fwd) checks for forward diagonal line, 
+		  else, checks for backwards diagonal */
+		this.checkDiagonal = function(fwd, val) {
+			var tempCells = fwd ? 
+				this.cells.slice().reverse() : this.cells;
+			for(var i = 0; i < this.len; i++) {
+				if(tempCells[i][i] != val) {
+					return false;
+				}
+			}
+			this.terminal = true;
+			return true;
+		}
+
+		//ckecks for orthogonal lines (vertical or horizontal)
+		this.checkOrthogonal = function(pos, val, vertical) {
+			for(var i = 0; i < this.len; i++) {
+				var cell = vertical ? 
+					this.cells[i][pos] :
+					this.cells[pos][i]
+				if(cell != val) {
+					return false;
+				}
+			}
+			this.terminal = true;
+			return true;
+		}
+
 		var res = {};
 		res.value = value;
+
 		//Diagonals
 		if(this.checkDiagonal(1, value)) {
 			res.orientation = "diagonal";
@@ -47,6 +52,7 @@ function Board(cells) {
 			res.position = 0;
 			return res;
 		}
+
 		//Orthogonals
 		for(var i = 0; i < this.len; i++){
 			//Vertical
@@ -66,14 +72,10 @@ function Board(cells) {
 	}
 }
 
-var cells = [   
-				[3, 1, 3, 1, 2, 3],
-				[1, 3, 3, 1, 3, 3],
-				[0, 0, 0, 0, 0, 0],
-				[3, 0, 0, 3, 2, 0],
-				[3, 0, 3, 1, 3, 3],
-				[3, 1, 3, 1, 2, 3],];
+var cells = [   [0, 6, 5],
+				[0, 5, 1],
+				[5, 6, 0]   ];
 
 var test = new Board(cells);
-
-console.log(test.checkLine(3));
+console.log(test.checkLine(6));
+console.log("Borad.terminal = " + test.terminal);
