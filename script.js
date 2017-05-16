154f8617faab7ee8f094754b9ed7b52344575d68
Halim <halim.bouguedra@gmail.com>
2017-05-16 14:55:05 +0100

    late bulk commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const NT = "nonterminal";
 const MAXI = 1;
 const MINI = -1;
 const TIE = 0;
+var cells;
 
 
 Board.prototype = {
@@ -108,18 +109,75 @@ function Board(cells, player) {
 			[] : this.getMoves(cells, player);
 }
 
+function minimax(board) {
+	if(board.terminal != NT) {
+		res = board.terminal;
+		if(res == TIE) {
+			res = board.player * -1 ;
+		}
+		return res;
+	}
+	else{
+		var pl = board.player;
+		var moves = board.possibleMoves;
+		var next = pl * -1;
+		var bestVal = (pl == MAXI) ? -1 : 1;
+		var best = (pl == MAXI) ? Math.max : Math.min;
+		for(var i = 0; i < moves.length; i++) {
+			var newBoard = new Board(moves[i], next);
+			bestVal += best(bestVal, minimax(newBoard));
+		}
+		return bestVal;
+	}
+}
 
-/*
+function nextMove(board) {
+	var	pl = board.player;
+	var moves = board.possibleMoves;
+	var values = [];
+	for(var i = 0; i < moves.length; i++) {
+		var b = new Board(moves[i], pl);
+		values.push(minimax(b));
+	}
+	var best = (pl == MAXI) ? 
+		Math.max(...values) : Math.min(...values);
+	// console.log("Math.max(values) = " + Math.max(...values));
+	// console.log("Math.min(values) = " + Math.min(...values));
+	var index = values.indexOf(best);
+	// console.log("best = " + best);
+	// console.log("index = " + index);
+	return moves[index]; 
+}
+ /*
+
 //TESTING
-var cells = [   [1, 2, 5],
-				[5, 1, 5],
-				[5, 1, 2]   ];
+var cells = [   [0, 0, 0],
+				[0, 0, 0],
+				[0, 0, 0]   ];
 
-var test = new Board(cells, 1);
-console.log("Borad.terminal: ");
-console.log(test.terminal);
-
-
+var testCases = []
+for(var i = 0; i < 9; i++) {
+	var temp = cells.map(function(arr){
+						return arr.slice();
+					});
+	temp[Math.floor(i / 3)][i % 3] = 1;
+	testCases.push(temp);
+}
+for(var i = 0; i < testCases.length; i++) {
+	var board = new Board(testCases[i], -1);
+	console.log("< < < < < TEST CASE : ");
+	printBoard(board.cells);
+	console.log("> > > > >\n")
+	while(board.terminal == NT) {
+		console.log("nextMove(board): ");
+		var nextM = nextMove(board);
+		printBoard(nextM);
+		var nextPl = board.player == MAXI ? MINI : MAXI;
+		board = new Board(nextM, nextPl);
+	}
+	console.log("\n\n\n");
+}
+/*
 for(var b = 0; b < 30; b++) {
 	var grid = []
 	for(var i = 0; i < 3; i++) {
@@ -133,12 +191,12 @@ for(var b = 0; b < 30; b++) {
 	var board = new Board(grid, 1);
 	console.log("< <")
 	printBoard(grid);
-	console.log("Borad.terminal: ");
-	console.log(board.terminal);
+	console.log("minimax(board): ");
+	console.log(minimax(board));
 	console.log(" > > \n\n")
 
 }
-
+*/
 function printBoard(board) {
 	var res = "";
 	for(var i = 0; i < 3; i++) {
@@ -149,4 +207,93 @@ function printBoard(board) {
 	}
 	console.log(res);
 }
-*/
+
+function init() {
+	var board = '';
+	for(var i = 0; i < 9; i++) {
+		var crossImg = '<img id="cross' + i + 
+		'" src="crosses/cross' + (i + 1) + '.svg" />';
+		var circleImg = '<img id="circle' + i + 
+		'" src="circles/circle' + (i + 1) + '.svg" />'
+		var cell = '<div class="cell" id="' + i + '">' + 
+		crossImg + circleImg + '</div>';
+		board += cell;
+	}
+	document.getElementById("board").innerHTML = board;
+
+	cells = [   [-1, 0, 1],
+				[0, 1, 0],
+				[1, 0, -1]   ];
+}
+
+document.addEventListener('click', handleClick);
+
+document.addEventListener("contextmenu", 
+	function(ev) {handleClick(ev, true); return false;})
+	
+function handleClick(e, right) {
+	var id = parseInt(e.target.id);
+	if(Number.isInteger(id)) {
+		
+		var cross = e.button;
+		var active = e.button ? "cross" : "circle";
+		var hidden = active == "cross" ? "circle" : "cross";
+		
+		var activeImg = document.getElementById(active + id);
+		var hiddenImg = document.getElementById(hidden + id);
+		
+		activeImg.style.visibility =
+		 (activeImg.style.visibility == "visible") ?
+		  "hidden" : "visible";
+		
+		hiddenImg.style.visibility =
+		 (hiddenImg.style.visibility == "hidden") ?
+		  "visible" : "hidden";
+		
+		cells = updateBoard(cells, id, (cross ? "-1" : "1"));
+		printBoard(cells);
+	}
+
+	else {
+		console.log("the target isn't a cell !")
+	}
+}
+
+function updateBoard(cells, id, value) {
+	cells[Math.floor(id / 3)][id % 3] = 1;
+	return cells;
+}
+
+function displayBoard(board) {
+	for(var i = 0; i < board.length; i++) {
+		for(var j = 0; j < board[i].length; j++) {
+			var id = i * 3 + j;
+			var cross = document.getElementById("cross" + id);
+			var circle = document.getElementById("circle" + id);
+			var crossVis, circleVis;
+			switch(board[i][j]) {
+				case 0:
+					crossVis = "hidden";
+					circleVis = "hidden";
+					break;
+				case 1:
+					crossVis = "visible";
+					circleVis = "hidden";
+					break;
+				case -1: 
+					crossVis = "hidden";
+					circleVis = "visible";
+					break;
+				default:
+					console.warn("illegal board value : " + board[i][j]);
+					break;
+			}
+			cross.style.visibility = crossVis;
+			circle.style.visibility = circleVis;
+		}
+	}
+}
+
+
+init();
+displayBoard(cells);
